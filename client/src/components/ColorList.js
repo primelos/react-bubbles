2aa54500d09841716e1662ec82a6a89c90437b6c
Ale <alearanibar@gmail.com>
2019-11-15 12:22:00 -0800

    add button

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -11,7 +11,10 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
-
+  const [newColor, setNewColor] = useState({
+    color: "",
+    code: { hex: "" }
+  })
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
@@ -41,9 +44,22 @@ const ColorList = ({ colors, updateColors }) => {
     .catch(err => console.log(err.res))
   };
 
+  const addColor = e => {
+    e.preventDefault()
+    axiosWithAuth()
+      .post('/colors', newColor)
+      .then(res => {
+        updateColors(res.data)
+        setNewColor({
+          color: '',
+          code: { hex: '' }})
+      })
+      .catch(err => console.log(err))
+  }
   return (
     <div className="colors-wrap">
       <p>colors</p>
+
       <ul>
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
@@ -64,6 +80,7 @@ const ColorList = ({ colors, updateColors }) => {
           </li>
         ))}
       </ul>
+
       {editing && (
         <form onSubmit={saveEdit}>
           <legend>edit color</legend>
@@ -94,8 +111,27 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
-      <div className="spacer" />
+      <div className="" />
       {/* stretch - build another form here to add a color */}
+      
+      <form onSubmit={addColor} >
+        <input 
+          type='text'
+          name='color'
+          placeholder='enter color'
+          value={newColor.color}
+          onChange={ e => setNewColor({...newColor, color: e.target.value})}
+        />
+        <input 
+          type='text'
+          name='code'
+          placeholder='enter code'
+          value={newColor.code.hex}
+          onChange={ e => setNewColor({ ...newColor, code: { hex: e.target.value }})}
+          
+        />
+        <button>enter</button>
+      </form>
     </div>
   );
 };
