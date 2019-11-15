9a2faae27c7bd58f9965e8183c5a8141c56e4191
Ale <alearanibar@gmail.com>
2019-11-15 08:15:18 -0800

    created state

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,6 +3,15 @@ import React from "react";
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [enter, setEnter] = useState({ username:'', password:'' })
+
+  const changeIt = e => {
+    e.preventDefault()
+    setEnter({
+      ...enter, [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
