0448e3c9f16d1a5091f6cf449eaed9da54e46e70
Ale <alearanibar@gmail.com>
2019-11-15 09:58:01 -0800

    put done

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -22,6 +22,14 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    // const pallet = colors.match.params.id
+
+    axiosWithAuth()
+      .put(`colors/${colorToEdit.id}`, colorToEdit//updateColors)
+      .then(resolve => {
+        colors.history.push('/protected')
+      })
+      .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
